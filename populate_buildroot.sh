949a453a60e80f25eb9530dda2f9732f06373c0b
gutelfuldead <gutel@hcs.ufl.edu>
2017-04-26 08:36:09 -0700

    updated populate_buildroot.sh

--- populate_buildroot.sh
+++ populate_buildroot.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-if [ "$1" = '' ]; then
-	BROOTPATH=../buildroot-2016.08-rc1
-else
-	BROOTPATH=$1
-fi
-
 echo ">>> Copying all files into $BROOTPATH ..."
 cp ./config_files/buildroot_config $BROOTPATH/.config
 cp ./config_files/package_config $BROOTPATH/package/Config.in
