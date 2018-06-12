7f6802f7f784b4bc140c232c170198165313416c
gutelfuldead <gutel@hcs.ufl.edu>
2018-06-11 21:07:17 -0700

    added check for buildroot path to populate_buildroot.sh

--- populate_buildroot.sh
+++ populate_buildroot.sh
@@ -2,8 +2,13 @@
 
 BROOTPATH=./buildroot-2018.02.2
 
-wget https://buildroot.org/downloads/buildroot-2018.02.2.tar.gz &&
-tar -xvzf buildroot-2018.02.2.tar.gz &&
+if [ ! -d $BROOTPATH ]; then
+	echo ">>> Buildroot path not found... Fetching buildroot..."
+	wget https://buildroot.org/downloads/buildroot-2018.02.2.tar.gz 
+	tar -xvzf buildroot-2018.02.2.tar.gz
+else
+	echo ">>> Buildroot path found..."
+fi
 
 echo ">>> Copying all files into $BROOTPATH"
 cp ./config_files/buildroot_config $BROOTPATH/.config
